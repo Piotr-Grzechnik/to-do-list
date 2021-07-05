f37a276b46a92e1f503964ebb24acd9e1a16e5b7
Piotr Grzechnik <86979944+Piotr-Grzechnik@users.noreply.github.com>
2021-07-06 00:59:02 +0200

    almost finish

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,14 @@
 {
     const tasks = [
         {
-            content: "Masło",
-            done: true,
+            content: "",
+            done: false,
         },
     ];
+    const ToogleTaskDone=(taskindex)=>{
+        tasks[taskindex].done=!tasks[taskindex].done;
+        Render();
+    };
     const AddNewTask = (newTask) => {
         tasks.push(
             {
@@ -15,19 +19,37 @@
     const Render = () => {
         let htmlString = "";
         for (const task of tasks) {
-            htmlString += `<li class="form__task"><button class="form__button--toggleDone">${task.done?"✔":""}</button>${task.content}<button class="form__button--deleteTask">🔙</button></li>`;
+            htmlString += `<li class="form__task-list"><button class="form__button--toggleDone js-done">${task.done?"✔":""}</button><p class="form__task ${task.done?"form__task-done":""}">${task.content}</p><button class="form__button--deleteTask js-remove">🗑</button></li>`;
         }
         document.querySelector(".js-tasksList").innerHTML = htmlString;
+        const removeButtons=document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButton, index)=>{
+            removeButton.addEventListener("click",()=>{
+                tasks.splice(index,1);
+                Render();
+            })
+        });
+        const toogleDoneButtons=document.querySelectorAll(".js-done");
+        toogleDoneButtons.forEach((toogleDoneButton, taskindex)=>{
+            toogleDoneButton.addEventListener("click",()=>{
+                ToogleTaskDone(taskindex);
+            })
+        });
     };
     const onFormSubmit = (event) => {
         event.preventDefault();
         const newTask = document.querySelector(".form__input").value;
-        if (newTask === "") {
+        if (newTask === ""){
+            document.querySelector(".form__input").focus();
             return;
         }
         AddNewTask(newTask);
+        document.querySelector(".form__input").value=null;
     };
     const init = () => {
+        onload=()=>{
+            document.querySelector(".js-tasksList").innerHTML =`<input class="form__input form__input--disabled" type="text" placeholder="Start adding new tasks..." disabled>`
+        };
         const form = document.querySelector(".js-form");
         form.addEventListener("submit", (onFormSubmit));
         Render();
